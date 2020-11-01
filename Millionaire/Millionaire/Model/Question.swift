706f36acb70a5798173667a46234f4f91a6513a3
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2020-11-01 13:28:32 +0300

    Lesson1

---
+++ Millionaire/Millionaire/Model/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Millionaire
+//
+//  Created by Mac on 28.10.2020.
+//
+
+import Foundation
+struct Question {
+    var question: String
+    var answer1: String
+    var answer2: String
+    var answer3: String
+    var answer4: String
+    var correctAnswer: String
+}
---
+++ Millionaire/Millionaire/VCs/MenuViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Mac on 28.10.2020.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+  
+    @IBOutlet weak var results: UIButton!
+    @IBAction func results(_ sender: UIButton) {}
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "toGameVC":
+            if let destination = segue.destination as? GameViewController {
+                destination.endGame = { [weak self] (score, numberQuestions) in
+                    guard self != nil else { return }
+                    let gameSession = GameSession(date: Date(), numberOfQuestions:numberQuestions, numberOfCorrectAnswers: score)
+                     Game.shared.addSession(gameSession: gameSession)
+                    self?.results.setTitle("Вы отгадали \(score) из \(numberQuestions) вопросов", for: .normal)
+                
+                }
+            }
+        
+        default:
+            break
+        }
+    }
+    
+    @IBAction func startButton(_ sender: UIButton) {
+             performSegue(withIdentifier: "toGameVC", sender: self)
+      
+       }
+}
+
+
