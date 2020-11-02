a8c19edef0ff914037fb2a93b0677a9c049bffe4
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2020-11-02 10:51:11 +0300

    Lesson2

---
+++ Millionaire/Millionaire/Strategy/ QuestionsInOrderStrategy.swift
@@ -0,0 +1,15 @@
+//
+//   QuestionsInOrderStrategy.swift
+//  Millionaire
+//
+//  Created by Mac on 01.11.2020.
+//
+
+import Foundation
+class QuestionsInOrderStrategy: QuestionStrategy {
+    func setQuestions(questions: [Question]) -> [Question] {
+        return questions
+    }
+    
+  
+}
